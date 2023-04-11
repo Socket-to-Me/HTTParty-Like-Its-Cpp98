2d2be63234b123adccdc06a768389a355d93c66b
123Untitled <123untitled@pm.me>
2023-04-12 01:41:19 +0200

    add main.cpp

---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include "Main.hpp"
+#include <iostream>
+
+int main(void) {
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Welcome to the webserv " << WEBSERV_VERSION << std::endl;
+
+	return EXIT_SUCCESS;
+}
