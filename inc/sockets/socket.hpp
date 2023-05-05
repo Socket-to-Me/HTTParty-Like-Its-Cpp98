746d49ff7c68cbe2649b912c4636a3e75ad6074c
123Untitled <123untitled@pm.me>
2023-05-06 00:21:51 +0200

    start socket implementation

--- inc/sockets/socket.hpp
+++ inc/sockets/socket.hpp
@@ -1,6 +1,86 @@
 #ifndef SOCKETS_HEADER
 #define SOCKETS_HEADER
 
+#include <sys/socket.h>
+#include <unistd.h>
+
+/* prototype */
+// int socket(int domain, int type, int protocol);
+
+// DESCRIPTION socket() creates an endpoint for communication and returns a descriptor.
+
+
+/* protocol */
+
+// PF_LOCAL        Host-internal protocols, formerly called PF_UNIX,
+// PF_UNIX         Host-internal protocols, deprecated, use PF_LOCAL,
+// PF_INET         Internet version 4 protocols,
+// PF_ROUTE        Internal Routing protocol,
+// PF_KEY          Internal key-management function,
+// PF_INET6        Internet version 6 protocols,
+// PF_SYSTEM       System domain,
+// PF_NDRV         Raw access to network device,
+// PF_VSOCK        VM Sockets protocols
+
+
+/* type */
+
+// SOCK_STREAM
+// SOCK_DGRAM
+// SOCK_RAW
+
 // -- N A M E S P A C E S  I R C ----------------------------------------------
+
+namespace IRC {
+
+
+	// -- S O C K E T  C L A S S ----------------------------------------------
+
+	class Socket {
+
+		public:
+
+			// -- P U B L I C  A L I A S E S ----------------------------------
+
+			/* descriptor type */
+			typedef int Descriptor;
+
+
+			// -- P U B L I C  C O N S T R U C T O R S ------------------------
+
+			/* default constructor */
+			Socket(void);
+
+			/* copy constructor */
+			Socket(const Socket &socket);
+
+			/* destructor */
+			~Socket(void);
+
+
+			// -- P U B L I C  M E T H O D S ----------------------------------
+
+			/* create socket */
+			void create(int domain, int type, int protocol);
+
+
+		private:
+
+			// -- P R I V A T E  E N U M S ------------------------------------
+
+			/* descriptor type */
+			enum {
+				NULL_DESCRIPTOR = -1
+			};
+
+			// -- P R I V A T E  M E M B E R S --------------------------------
+
+			/* descriptor */
+			mutable Descriptor _descriptor;
+
+
+	};
+
+}
 
 #endif
--- src/sockets/socket.cpp
+++ src/sockets/socket.cpp
@@ -1,1 +1,37 @@
-#include "sockets.hpp"
+#include "socket.hpp"
+
+/* default constructor */
+IRC::Socket::Socket(void)
+: _descriptor(NULL_DESCRIPTOR) {
+	// code here...
+}
+
+/* copy constructor */ // INFO: implemented like move constructor
+IRC::Socket::Socket(const Socket& other)
+: _descriptor(other._descriptor) {
+	// invalidate other descriptor
+	other._descriptor = NULL_DESCRIPTOR;
+	// WARNING: this maybe not be public
+	// does socket copy make sense ?
+}
+
+/* destructor */
+IRC::Socket::~Socket(void) {
+	// check descriptor validity
+	if (_descriptor != NULL_DESCRIPTOR) {
+		// close socket
+		::close(_descriptor);
+	}
+}
+
+
+/* socket */
+void IRC::Socket::create(int domain, int type, int protocol) {
+	// check descriptor validity
+	if (_descriptor != NULL_DESCRIPTOR) {
+		// close socket
+		::close(_descriptor);
+	}
+	// create socket
+	_descriptor = ::socket(AF_INET, SOCK_STREAM, 0);
+}
