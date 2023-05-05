0ed3e7761e8e046115095daf77261e4329b918e1
123Untitled <123untitled@pm.me>
2023-05-06 00:22:02 +0200

    add authorized functions

--- inc/main.hpp
+++ inc/main.hpp
@@ -3,4 +3,11 @@
 
 #define WEBSERV_VERSION "0.1.0"
 
+// autorised functions
+// socket, close, setsockopt, getsockname,
+// getprotobyname, gethostbyname, getaddrinfo,
+// freeaddrinfo, bind, connect, listen, accept,
+// htons, htonl, ntohs, ntohl, inet_addr, inet_ntoa,
+// send, recv, signal, lseek, fstat, fcntl, poll (ou équivalent)
+
 #endif
