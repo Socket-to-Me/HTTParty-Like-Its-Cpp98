e593ffb36c4178b01b325c8ba55ab50f217b180d
123Untitled <123untitled@pm.me>
2023-05-06 19:12:38 +0200

    now socket act like shared_pointer

--- src/sockets/socket.cpp
+++ src/sockets/socket.cpp
@@ -1,37 +1,85 @@
 #include "socket.hpp"
 
+
+// -- P U B L I C  C O N S T R U C T O R S ------------------------------------
+
 /* default constructor */
 IRC::Socket::Socket(void)
-: _descriptor(NULL_DESCRIPTOR) {
+: _descriptor(NULL_DESCRIPTOR), _count(NULL) {
 	// code here...
 }
 
-/* copy constructor */ // INFO: implemented like move constructor
+/* copy constructor */
 IRC::Socket::Socket(const Socket& other)
-: _descriptor(other._descriptor) {
-	// invalidate other descriptor
-	other._descriptor = NULL_DESCRIPTOR;
-	// WARNING: this maybe not be public
-	// does socket copy make sense ?
+: _descriptor(other._descriptor), _count(other._count) {
+	// check count validity
+	if (_count) {
+		// increment number of references
+		++(*_count);
+	}
 }
 
 /* destructor */
 IRC::Socket::~Socket(void) {
-	// check descriptor validity
-	if (_descriptor != NULL_DESCRIPTOR) {
-		// close socket
-		::close(_descriptor);
+	// check socket end of life
+	if (_count && (--(*_count) == 0)) {
+		// check descriptor validity
+		if (_descriptor != NULL_DESCRIPTOR) {
+			// close socket
+			::close(_descriptor);
+			// invalidate descriptor
+			_descriptor = NULL_DESCRIPTOR;
+		} // deallocate count
+		delete _count;
+		// invalidate count
+		_count = nullptr;
 	}
 }
 
 
-/* socket */
-void IRC::Socket::create(int domain, int type, int protocol) {
-	// check descriptor validity
-	if (_descriptor != NULL_DESCRIPTOR) {
-		// close socket
-		::close(_descriptor);
-	}
-	// create socket
-	_descriptor = ::socket(AF_INET, SOCK_STREAM, 0);
+// -- P U B L I C  O P E R A T O R S ------------------------------------------
+
+/* copy assignment operator */
+IRC::Socket& IRC::Socket::operator=(const Socket& socket) {
+	// check self assignment
+	if (this != &socket) {
+		// call destructor
+		this->~Socket();
+		// copy descriptor
+		_descriptor = socket._descriptor;
+		_count      = socket._count;
+		// check count validity
+		if (_count) {
+			// increment number of references
+			++(*_count); }
+	} // return self reference
+	return *this;
 }
+
+/* bool operator */
+IRC::Socket::operator bool(void) const {
+	// check descriptor validity
+	return _descriptor != NULL_DESCRIPTOR;
+}
+
+/* not operator */
+bool IRC::Socket::operator!(void) const {
+	// check descriptor validity
+	return _descriptor == NULL_DESCRIPTOR;
+}
+
+/* equality operator */
+bool IRC::Socket::operator==(const Socket& socket) const {
+	// check descriptors equality
+	return _descriptor == socket._descriptor;
+}
+
+/* inequality operator */
+bool IRC::Socket::operator!=(const Socket& socket) const {
+	// check descriptors inequality
+	return (_descriptor != socket._descriptor);
+}
+
+
+// -- P U B L I C  M E T H O D S ----------------------------------------------
+
