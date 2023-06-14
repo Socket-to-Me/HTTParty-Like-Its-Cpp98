91b302218d449ec2be174318e3846294a62541a8
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-14 14:35:39 +0200

    add constructor message

---
+++ src/cmd/join.cpp
@@ -0,0 +1,27 @@
+#include "join.hpp"
+
+/* default constructor */
+irc::join::join(void) {
+	std::cout << "NEW JOIN INSTANCE" << std::endl;
+	return;
+}
+
+/* destructor */
+irc::join::~join(void) {
+	return;
+}
+
+/* execute command */
+bool irc::join::execute(void) {
+	return false;
+}
+
+/* evaluate command */
+bool irc::join::evaluate(void) {
+	return false;
+}
+
+/* create command */
+irc::auto_ptr<irc::cmd> irc::join::create(void) {
+	return irc::auto_ptr<irc::cmd>(new irc::join());
+}
