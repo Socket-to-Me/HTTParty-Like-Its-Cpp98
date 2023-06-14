d2805462e78b414fc13a2e86748b6139e034f28d
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-14 14:34:45 +0200

    update lookup lexer implementation

--- inc/lexer.hpp
+++ inc/lexer.hpp
@@ -4,116 +4,393 @@
 #include <sstream>
 #include <array>
 
-namespace IRC {
 
-	enum token_type {
+// -- I R C  N A M E S P A C E ------------------------------------------------
 
-		MAX_TOKEN_TYPE
+namespace irc {
+
+
+	// -- T O K E N -----------------------------------------------------------
+
+    class token {
+
+
+        public:
+
+			// -- public types ------------------------------------------------
+
+			/* size type */
+			typedef std::string::size_type size_type;
+
+
+			// -- public enums ------------------------------------------------
+
+			/* token type */
+			enum type {
+				UNDEFINED_TOKEN,
+				TAG_TOKEN,
+				SOURCE_TOKEN,
+				COMMAND_TOKEN,
+				END_TOKEN,
+				MAX_TOKEN_TYPE
+			};
+
+
+			// -- public constructors -----------------------------------------
+
+			/* c-string and type constructor */
+            token(const char*, const size_type, const type);
+
+			/* string and type constructor */
+			token(const std::string&, const type);
+
+			/* copy constructor */
+			token(const token&);
+
+			/* destructor */
+			~token(void);
+
+
+			// -- public assignment operator ----------------------------------
+
+			/* copy assignment operator */
+			token& operator=(const token&);
+
+
+			// -- public accessors --------------------------------------------
+
+			/* get token value */
+            const std::string& value(void) const;
+
+			/* get token type */
+            enum type type(void) const;
+
+
+			// -- public setters ----------------------------------------------
+
+			/* set token value */
+			void value(const std::string&);
+
+			/* set token value with c-string */
+			void value(const char*, const size_type);
+
+			/* set token type */
+			void type(const enum type);
+
+
+        private:
+
+			// -- private members ---------------------------------------------
+
+			/* token value */
+			std::string _value;
+
+			/* token type */
+			enum type _type;
+
+    };
+
+
+	// -- T O K E N  T Y P E --------------------------------------------------
+
+	/* token type helper */
+	typedef enum token::type token_type;
+
+
+
+	// -- M E S S A G E -------------------------------------------------------
+
+	class message {
+
+
+		public:
+
+			// -- public types ------------------------------------------------
+
+			/* size type */
+			typedef std::vector<token>::size_type size_type;
+
+
+			// -- public constructors -----------------------------------------
+
+			/* default constructor */
+			message(void);
+
+			/* copy constructor */
+			message(const message&);
+
+			/* destructor */
+			~message(void);
+
+
+			// -- public assignment operator ----------------------------------
+
+			/* copy assignment operator */
+			message& operator=(const message&);
+
+
+			// -- public subscript operator -----------------------------------
+
+			/* subscript operator */
+			const token& operator[](const size_type) const;
+
+
+			// -- public accessors --------------------------------------------
+
+			/* get vector size */
+			size_type size(void) const;
+
+
+			// -- public modifiers --------------------------------------------
+
+			/* append token */
+			void append(const token&);
+
+			/* append token with c-string value and type */
+			void append(const char*, const token::size_type, const token_type);
+
+			/* append token with string value and type */
+			void append(const std::string&, const token_type);
+
+
+		private:
+
+			// -- private members ---------------------------------------------
+
+			/* token vector */
+			std::vector<token> _tokens;
+
 	};
 
+
+	// -- M E S S A G E  L I S T ----------------------------------------------
+
+	/* message vector type */
+	typedef std::vector<message> message_list;
+
+
+
+
+	// -- L E X E R  S T A T E ------------------------------------------------
+
 	enum lexer_state {
-
-		DEFAULT,
-
-		IN_TAG,
-
-		IN_SOURCE,
-
-		START_CMD,
-		IN_CMD,
-
-
+		DEFAULT_STATE,
+		TAG_STATE,
+		SOURCE_STATE,
+		COMMAND_STATE,
 		MAX_LEXER_STATE
 	};
 
 
-	enum char_type {
+	// -- C H A R  T Y P E ----------------------------------------------------
 
-		AT,                 // @
-		COLON, // :
-		SPACE, // ' '
-		NEWLINE, // \n
-		CARRIAGE_RETURN, // \r
-		OTHER, // all other characters
+	enum char_type {
+		NULL_CHAR,       // \0
+		CTRL_CHAR,       // \1 - \31 (except \n and \r)
+
+		OTHER_CHAR,      // all other characters
+		COLON_CHAR,      // :
+		AT_CHAR,         // @
+
+		SPACE_CHAR,      // ' '
+		NL_CHAR,         // \n
+		CR_CHAR,         // \r
+
 		MAX_CHAR_TYPE
 	};
 
+
+	// -- L E X E R  A C T I O N ----------------------------------------------
+
 	enum lexer_action {
 		SKIP,
-		CATCH,
+		COUNT,
+		APPEND,
+		END_OF_MESSAGE,
 		MAX_LEXER_ACTION
 	};
 
 
+	// -- L E X E R -----------------------------------------------------------
+
 	class lexer {
 
         // treating UTF8 need to take in char as unsigned char
-        char_type   getCharType(uint8_t c);
+        char_type   get_char_type(uint8_t c);
 
 		struct transition {
 			lexer_state		state;
 			lexer_action	action;
 		};
 
-		std::array<std::array<transition,
-							MAX_CHAR_TYPE>,
-				MAX_LEXER_STATE> _lookup = {
+		public:
 
-			// -- D E F A U L T  S T A T E ----------------------------------------
+			// -- public static methods ---------------------------------------
 
-				{ IN_TAG, SKIP },  // AT
-				{ DEFAULT, SKIP }, // COLON
-				{ DEFAULT, SKIP }, // SPACE
-				{ DEFAULT, SKIP }, // NEWLINE
-				{ DEFAULT, SKIP }, // CARRIAGE_RETURN
-				{ DEFAULT, SKIP }  // OTHER
-		};
+			/* static lex */
+			static void lex(message_list&, const std::string&);
 
 
+		private:
 
-		transition trans = _lookup[current_state][current_char];
+			// -- private constructors ----------------------------------------
+
+			/* default constructor */
+			lexer(message_list&, const std::string&);
+
+			/* copy constructor */
+			lexer(const lexer&);
+
+			/* destructor */
+			~lexer(void);
+
+
+			// -- private methods ---------------------------------------------
+
+			/* lex */
+			void lex(void);
+
+			/* skip action */
+			void skip(void);
+
+			/* count action */
+			void count(void);
+
+			/* append action */
+			void append(void);
+
+			/* end of message action */
+			void end_of_message(void);
+
+
+
+
+
+
+			// -- T R A N S I T I O N -----------------------------------------
+
+			class transition {
+
+				public:
+
+					// -- public constructors ---------------------------------
+
+					/* members constructors */
+					transition(const lexer_state, const token_type, const lexer_action);
+
+					/* copy constructor */
+					transition(const transition&);
+
+					/* destructor */
+					~transition(void);
+
+
+					// -- public assignment operator --------------------------
+
+					/* copy assignment operator */
+					transition& operator=(const transition&);
+
+
+					// -- public accessors ------------------------------------
+
+					/* get lexer state */
+					lexer_state state(void) const;
+
+					/* get token type */
+					token_type type(void) const;
+
+					/* get lexer action */
+					lexer_action action(void) const;
+
+
+				private:
+
+					// -- private members -------------------------------------
+
+					/* state */
+					lexer_state _state;
+
+					/* token type */
+					token_type _type;
+
+					/* action */
+					lexer_action _action;
+
+			};
+
+
+			// -- private types -----------------------------------------------
+
+			/* lookup table type */
+			typedef std::array<std::array<transition,
+					           MAX_CHAR_TYPE>,
+					MAX_LEXER_STATE> table;
+
+			/* action prototype */
+			typedef void(lexer::*proto)(void);
+
+			/* action table type */
+			typedef std::array<proto, MAX_LEXER_ACTION> action;
+
+			/* char table type */
+			typedef std::array<char_type, 256> char_table;
+
+			/* size type */
+			typedef std::string::size_type size_type;
+
+			/* state debug type */
+			typedef std::array<std::string, MAX_LEXER_STATE> state_debug;
+
+			/* action debug type */
+			typedef std::array<std::string, MAX_LEXER_ACTION> action_debug;
+
+			/* char debug type */
+			typedef std::array<std::string, MAX_CHAR_TYPE> char_debug;
+
+
+			// -- private members ---------------------------------------------
+
+			/* message list reference */
+			message_list&      _msgs;
+
+			/* string to lex reference */
+			const std::string& _str;
+
+			/* index */
+			size_type          _idx;
+
+			/* counter */
+			std::size_t        _count;
+
+			/* transition */
+			transition         _transition;
+
+
+			// -- private static members --------------------------------------
+
+			/* lookup table */
+			static const table _lookup;
+
+			/* action table */
+			static const action _actions;
+
+			/* char table */
+			static const char_table _chars;
+
+			/* state debug */
+			static const state_debug _state_debug;
+
+			/* action debug */
+			static const action_debug _action_debug;
+
+			/* char debug */
+			static const char_debug _char_debug;
+
+
+
+
 
 	};
 
-
-
-
-
-
-
-
-    enum TokenType {
-
-        COMMAND,
-        SPACE,
-        PARAMETER,
-        TRAILING,
-        NEWLINE,
-        EOF_TOKEN,
-        ERROR
-    };
-
-    class Token {
-
-        public:
-            Token(TokenType type, const std::string& value);
-            TokenType               getType(void) const;
-            const std::string&      getValue(void) const;
-
-        private:
-            TokenType       _type;
-            std::string     _value;
-    };
-
-    class Lexer {
-
-        public:
-            Lexer(const std::string& text);
-            Token getNextToken();
-
-        private:
-            std::string _text;
-            size_t      _pos;
-    };
 
 }
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,6 +1,6 @@
 #include "lexer.hpp"
 
-char_type IRC::Lexer::getCharType(uint8_t c) {
+irc::char_type irc::lexer::get_char_type(uint8_t c) {
 
     switch (c) {
         case '@':
@@ -18,119 +18,518 @@ char_type IRC::Lexer::getCharType(uint8_t c) {
     }
 }
 
-IRC::Token::Token(TokenType type, const std::string& value)
-: _type(type), _value(value) 
-{
-	return ;
+
+
+
+// -- N E W  L E X E R --------------------------------------------------------
+
+
+/* look up table */
+const irc::lexer::table irc::lexer::_lookup = {
+	{
+
+		// -- D E F A U L T  S T A T E ----------------------------------------
+		{
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, COUNT), // NULL
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, COUNT), // CTRL
+
+			transition(COMMAND_STATE,  token::UNDEFINED_TOKEN, COUNT), // OTHER
+			transition(SOURCE_STATE,   token::UNDEFINED_TOKEN, COUNT), // COLON
+			transition(TAG_STATE,      token::UNDEFINED_TOKEN, COUNT), // AT
+
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, SKIP), // SPACE
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, SKIP), // NEWLINE
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, SKIP), // CARRIAGE_RETURN
+
+		},
+
+		// -- T A G  S T A T E ------------------------------------------------
+		{
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, COUNT), // NULL
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, COUNT), // CTRL
+
+			transition(TAG_STATE,      token::UNDEFINED_TOKEN, COUNT), // OTHER
+			transition(TAG_STATE,      token::UNDEFINED_TOKEN, COUNT), // COLON
+			transition(TAG_STATE,      token::UNDEFINED_TOKEN, COUNT), // AT
+
+			transition(DEFAULT_STATE,  token::TAG_TOKEN,       APPEND), // SPACE
+			transition(DEFAULT_STATE,  token::TAG_TOKEN,       APPEND), // NEWLINE
+			transition(DEFAULT_STATE,  token::TAG_TOKEN,       APPEND), // CARRIAGE_RETURN
+
+		},
+
+		// -- S O U R C E  S T A T E ------------------------------------------
+		{
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, COUNT), // NULL
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, COUNT), // CTRL
+
+			transition(SOURCE_STATE,   token::UNDEFINED_TOKEN, COUNT), // OTHER
+			transition(SOURCE_STATE,   token::UNDEFINED_TOKEN, COUNT), // COLON
+			transition(SOURCE_STATE,   token::UNDEFINED_TOKEN, COUNT), // AT
+
+			transition(DEFAULT_STATE,  token::SOURCE_TOKEN,    APPEND), // SPACE
+			transition(DEFAULT_STATE,  token::SOURCE_TOKEN,    APPEND), // NEWLINE
+			transition(DEFAULT_STATE,  token::SOURCE_TOKEN,    APPEND), // CARRIAGE_RETURN
+
+		},
+
+		// -- C O M M A N D  S T A T E ----------------------------------------
+		{
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, COUNT), // NULL
+			transition(DEFAULT_STATE,  token::UNDEFINED_TOKEN, COUNT), // CTRL
+
+			transition(COMMAND_STATE,  token::UNDEFINED_TOKEN, COUNT), // OTHER
+			transition(COMMAND_STATE,  token::UNDEFINED_TOKEN, COUNT), // COLON
+			transition(COMMAND_STATE,  token::UNDEFINED_TOKEN, COUNT), // AT
+
+			transition(DEFAULT_STATE,  token::COMMAND_TOKEN,   APPEND), // SPACE
+			transition(DEFAULT_STATE,  token::COMMAND_TOKEN,   APPEND), // NEWLINE
+			transition(DEFAULT_STATE,  token::COMMAND_TOKEN,   APPEND), // CARRIAGE_RETURN
+
+		},
+
+
+
+	}
+
+};
+
+/* action table */
+const irc::lexer::action irc::lexer::_actions = {
+	&irc::lexer::skip,           // SKIP
+	&irc::lexer::count,          // COUNT
+	&irc::lexer::append,         // APPEND
+	&irc::lexer::end_of_message, // END_OF_MESSAGE
+};
+
+/* char table (256) */
+const irc::lexer::char_table irc::lexer::_chars = {
+	// 0x00 null
+	NULL_CHAR,
+	// 0x01 - 0x09 ctrl
+	CTRL_CHAR, CTRL_CHAR, CTRL_CHAR, CTRL_CHAR,
+	CTRL_CHAR, CTRL_CHAR, CTRL_CHAR, CTRL_CHAR,
+	CTRL_CHAR,
+	// 0x0A newline
+	NL_CHAR,
+	// 0x0B - 0x0C ctrl
+	CTRL_CHAR, CTRL_CHAR,
+	// 0x0D carriage return
+	CR_CHAR,
+	// 0x0E - 0x1F ctrl
+	CTRL_CHAR, CTRL_CHAR, CTRL_CHAR, CTRL_CHAR,
+	CTRL_CHAR, CTRL_CHAR, CTRL_CHAR, CTRL_CHAR,
+	CTRL_CHAR, CTRL_CHAR, CTRL_CHAR, CTRL_CHAR,
+	CTRL_CHAR, CTRL_CHAR, CTRL_CHAR, CTRL_CHAR,
+	// 0x20 space
+	SPACE_CHAR,
+	// 0x21 - 0x39 other
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	// 0x3A colon
+	COLON_CHAR,
+	// 0x3B - 0x3F other
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR,
+	// 0x40 at
+	AT_CHAR,
+	// 0x41 - 0xFF other
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR, OTHER_CHAR,
+	OTHER_CHAR, OTHER_CHAR, OTHER_CHAR
+};
+
+
+/* state debug */
+const irc::lexer::state_debug irc::lexer::_state_debug = {
+	"DEFAULT_STATE",
+	"TAG_STATE",
+	"SOURCE_STATE",
+	"COMMAND_STATE"
+};
+
+/* action debug */
+const irc::lexer::action_debug irc::lexer::_action_debug = {
+	"SKIP",
+	"COUNT",
+	"APPEND",
+	"END_OF_MESSAGE"
+};
+
+/* char debug */
+const irc::lexer::char_debug irc::lexer::_char_debug = {
+	"NULL",
+	"CTRL",
+	"OTHER",
+	"COLON",
+	"AT",
+	"SPACE",
+	"NEWLINE",
+	"CARRIAGE_RETURN",
+};
+
+
+// -- T R A N S I T I O N -----------------------------------------------------
+
+
+// -- public constructors -----------------------------------------------------
+
+/* default constructor */
+irc::lexer::transition::transition(const irc::lexer_state state, const irc::token_type type, const irc::lexer_action action)
+: _state(state), _type(type), _action(action) {
+	// nothing to do...
 }
 
-IRC::TokenType               IRC::Token::getType(void) const
-{
+/* copy constructor */
+irc::lexer::transition::transition(const irc::lexer::transition& other)
+: _state(other._state), _type(other._type), _action(other._action) {
+	// nothing to do...
+}
+
+/* destructor */
+irc::lexer::transition::~transition(void) {
+	// nothing to do...
+}
+
+
+// -- public assignment operator ----------------------------------------------
+
+/* copy assignment operator */
+irc::lexer::transition& irc::lexer::transition::operator=(const irc::lexer::transition& other) {
+	// copy data members
+	_state  = other._state;
+	_type   = other._type;
+	_action = other._action;
+	// return self reference
+	return *this;
+}
+
+// -- public accessors --------------------------------------------------------
+
+/* get lexer state */
+irc::lexer_state irc::lexer::transition::state(void) const {
+	// return state
+	return _state;
+}
+
+/* get token type */
+irc::token_type irc::lexer::transition::type(void) const {
+	// return type
 	return _type;
 }
 
-const std::string&      IRC::Token::getValue(void) const
-{
+/* get lexer action */
+irc::lexer_action irc::lexer::transition::action(void) const {
+	// return action
+	return _action;
+}
+
+
+
+
+// -- L E X E R ---------------------------------------------------------------
+
+
+// -- private constructors ----------------------------------------------------
+
+/* default constructor */
+irc::lexer::lexer(message_list& msgs, const std::string& str)
+: _msgs(msgs), _str(str),
+  _idx(0), _count(0),
+  _transition(DEFAULT_STATE, token::UNDEFINED_TOKEN, SKIP) {
+	// nothing to do...
+}
+
+/* copy constructor */
+irc::lexer::lexer(const irc::lexer& other)
+: _msgs(other._msgs), _str(other._str),
+  _idx(other._idx), _count(other._count),
+  _transition(other._transition) {
+	// nothing to do...
+}
+
+/* destructor */
+irc::lexer::~lexer(void) {
+	// nothing to do...
+}
+
+
+// -- public static methods ---------------------------------------------------
+
+/* static lex */
+void irc::lexer::lex(message_list& msgs, const std::string& to_lex) {
+	// instantiate lexer and call lex method
+	lexer(msgs, to_lex).lex();
+}
+
+
+
+// -- private methods ---------------------------------------------------------
+
+/* lex */
+void irc::lexer::lex(void) {
+
+	// clear previous message list
+	_msgs.clear();
+
+	// check if string is empty
+	if (_str.empty()) { return; }
+
+	do {
+
+		// get current character (need implementation)
+		char_type ct = _chars[_str[_idx]];
+
+		// debug
+		std::cout << "current character: " << _str[_idx] << " (" << _char_debug[ct] << ")\n"
+				  << "    current state: " << _state_debug[_transition.state()] << "\n";
+
+
+		// get transition
+		_transition = _lookup[_transition.state()][ct];
+
+		// debug
+		std::cout << "       next state: " << _state_debug[_transition.state()] << "\n"
+				  << "      next action: " << _action_debug[_transition.action()] << "\n\n";
+
+
+		// execute action
+		(this->*_actions[_transition.action()])();
+
+
+	} while (_idx++ < _str.size());
+
+
+}
+
+/* skip */
+void irc::lexer::skip(void) {
+	// do nothing...
+}
+
+/* count */
+void irc::lexer::count(void) {
+	// increment count
+	++_count;
+}
+
+/* append */
+void irc::lexer::append(void) {
+	// append character to current token
+	//_tokens.back().append(_str[_idx]);
+	_count = 0;
+}
+
+/* end of message */
+void irc::lexer::end_of_message(void) {
+
+}
+
+
+
+// -- T O K E N ---------------------------------------------------------------
+
+
+// -- public constructors -----------------------------------------------------
+
+/* c-string and type constructor */
+irc::token::token(const char* value, const irc::token::size_type size, const irc::token_type type)
+: _value(value, size), _type(type) {
+	// nothing to do...
+}
+
+/* string and type constructor */
+irc::token::token(const std::string& value, const irc::token_type type)
+: _value(value), _type(type) {
+	// nothing to do...
+}
+
+/* copy constructor */
+irc::token::token(const irc::token& other)
+: _value(other._value), _type(other._type) {
+	// nothing to do...
+}
+
+/* destructor */
+irc::token::~token(void) {
+	// nothing to do...
+}
+
+
+// -- public assignment operator ----------------------------------------------
+
+/* copy assignment operator */
+irc::token& irc::token::operator=(const irc::token& other) {
+	// copy data members
+	_value = other._value;
+	_type  = other._type;
+	// return self reference
+	return *this;
+}
+
+
+// -- public accessors --------------------------------------------------------
+
+/* get value */
+const std::string& irc::token::value(void) const {
+	// return value
 	return _value;
 }
 
-IRC::Lexer::Lexer(const std::string& text)
-: _text(text), _pos(0) {
-
-	return ;
+/* get type */
+irc::token_type irc::token::type(void) const {
+	// return type
+	return _type;
 }
 
-IRC::Token	IRC::Lexer::getNextToken(void) {
 
-	while (_pos < _text.size()) {
+// -- public setters ----------------------------------------------------------
 
-		std::string	str;
-		bool		err = false;
-
-		// ----- C O M M A N D ------------------------------------
-
-		if (isalpha(_text[_pos])) {
-
-			bool		caps = true;
-
-			while (isalpha(_text[_pos])) {
-
-				if (islower(_text[_pos]))
-					caps = false;
-		
-				str += _text[_pos];
-				++_pos;
-			}
-
-			if (caps)
-				return Token(COMMAND, str);
-
-			return Token(PARAMETER, str);
-		}
-
-		// ----- T R A I L I N G ------------------------------------
-
-		if (_text[_pos] == ':') {
-
-			str += _text[_pos++];
-
-			while (_text[_pos] && _text[_pos] != '\r' && _text[_pos] != '\n' && _text[_pos] != '\0') {
-
-				if (_text[_pos] == ':') 
-					err = true;
-				
-				str += _text[_pos];
-				++_pos;
-			}
-
-			if (err)
-				return Token(ERROR, str);
-			return Token(TRAILING, str);
-
-		}
-
-		// ----- S P A C E ------------------------------------
-
-		if (_text[_pos] == ' ') {
-
-			str += _text[_pos];
-			++_pos;
-			return Token(SPACE, str);
-		}
-
-		// ----- N E W  L I N E ------------------------------------
-
-		if (_text[_pos] == '\n') {
-
-			str += _text[_pos];
-			++_pos;
-			return Token(NEWLINE, str);
-		}
-	
-		// ----- E O F ------------------------------------
-
-		if (_text[_pos] == '\r' && _text[_pos+1] == '\n') {
-
-			++_pos;
-			++_pos;
-			return Token(EOF_TOKEN, "");
-		}
-
-		// ----- P A R A M E T E R S ------------------------------------
-
-		while (_text[_pos] && _text[_pos] != ' ' && _text[_pos] != '\r' && _text[_pos] != '\n') {
-
-			if (_text[_pos] == ':') 
-				err = true;
-	
-			str += _text[_pos];
-			++_pos;
-		}
-
-		if (err)
-			return Token(ERROR, str);
-		return Token(PARAMETER, str);
-	}
-
-	return Token(EOF_TOKEN, "");
+/* set value */
+void irc::token::value(const std::string& value) {
+	// set value
+	_value.assign(value);
 }
+
+/* set value with c-string */
+void irc::token::value(const char* value, const irc::token::size_type size) {
+	// set value
+	_value.assign(value, size);
+}
+
+/* set type */
+void irc::token::type(const irc::token_type type) {
+	// set type
+	_type = type;
+}
+
+
+
+
+// -- M E S S A G E -----------------------------------------------------------
+
+// -- public constructors -----------------------------------------------------
+
+/* default constructor */
+irc::message::message(void)
+: _tokens() {
+	// nothing to do...
+}
+
+/* copy constructor */
+irc::message::message(const irc::message& other)
+: _tokens(other._tokens) {
+	// nothing to do...
+}
+
+/* destructor */
+irc::message::~message(void) {
+	// nothing to do...
+}
+
+
+// -- public assignment operator ----------------------------------------------
+
+/* copy assignment operator */
+irc::message& irc::message::operator=(const irc::message& other) {
+	// check for self assignment
+	if (this != &other) {
+		// copy data members
+		_tokens = other._tokens;
+	} // return self reference
+	return *this;
+}
+
+
+// -- public subscript operator -----------------------------------------------
+
+/* subscript operator */
+const irc::token& irc::message::operator[](const size_type idx) const {
+	// return token
+	return _tokens[idx];
+}
+
+
+// -- public accessors --------------------------------------------------------
+
+/* get size */
+irc::message::size_type irc::message::size(void) const {
+	// return size
+	return _tokens.size();
+}
+
+
+// -- public modifiers --------------------------------------------------------
+
+/* append token */
+void irc::message::append(const irc::token& token) {
+	// append token
+	_tokens.push_back(token);
+}
+
+/* append token with c-string value and type */
+void irc::message::append(const char* value, const irc::token::size_type size, const irc::token_type type) {
+	// append token
+	_tokens.push_back(token(value, size, type));
+}
+
+/* append token with string value and type */
+void irc::message::append(const std::string& value, const irc::token_type type) {
+	// append token
+	_tokens.push_back(token(value, type));
+}
+
+
+
+
+
