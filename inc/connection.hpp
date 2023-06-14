456114b73ea957cee07a776868f73b7957b1f45e
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-14 17:37:05 +0200

    modify connection to handle event buffer

--- inc/connection.hpp
+++ inc/connection.hpp
@@ -9,23 +9,84 @@
 #include <vector>
 #include <poll.h>
 
+
+// -- I R C  N A M E S P A C E ------------------------------------------------
+
 namespace IRC {
+
+
+	// -- C O N N E C T I O N -------------------------------------------------
 
     class Connection {
 
-        private:
-
-            const struct pollfd& _pfd;
 
         public:
 
-            Connection(const struct pollfd& pfd);
-        
-            bool    	hasEventOccured(void);
-            ssize_t    	send(const std::string& message);
-            ssize_t		receive(char* buffer, int buffer_size);
-            void        close(void);
-            int         getfd(void);
+			// -- public constructors -----------------------------------------
+
+			/* pollfd reference constructor */
+            Connection(const struct pollfd&);
+
+			/* copy constructor */
+			Connection(const Connection&);
+
+			/* destructor */
+			~Connection(void);
+
+
+		private:
+
+			// -- private assignment operator ----------------------------------
+
+			/* copy assignment operator */
+			Connection& operator=(const Connection&);
+
+
+		public:
+
+			// -- public methods ----------------------------------------------
+
+			/* receive bytes */
+            bool receive(void);
+
+			/* send bytes */
+            ssize_t send(const std::string& message);
+
+			/* close connection */
+            void close(void);
+
+			/* extract message */
+			std::string extract_message(void);
+
+
+			// -- public accessors --------------------------------------------
+
+			/* get file descriptor */
+            int getfd(void);
+
+
+        private:
+
+			// -- private methods ---------------------------------------------
+
+			/* check end of message */
+			bool check_crlf(void);
+
+
+			// -- private enums -----------------------------------------------
+
+			/* buffer size */
+			enum { BUFFER_SIZE = 1024 };
+
+
+			// -- private members ---------------------------------------------
+
+			/* pollfd reference */
+            const struct pollfd& _pfd;
+
+			/* buffer */
+			std::string _buffer;
+
 
     };
 
