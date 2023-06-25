e3f53772ef802e3cf699477ce7da684ebe385586
123Untitled <78928284+123Untitled@users.noreply.github.com>
2023-06-25 22:31:26 +0200

    add parser

--- inc/parser.hpp
+++ inc/parser.hpp
@@ -1,17 +1,162 @@
+#ifndef PARSER_HEADER
+#define PARSER_HEADER
+
 #include "lexer.hpp"
+
 
 namespace irc {
 
-//    class Parser {
-//
-//        public:
-//
-//            Parser(const Lexer& lexer);
-//            std::vector< std::vector< irc::Token > > parse(void);
-//
-//        private:
-//
-//            Lexer   _lexer;
-//            Token   _currToken;
-//    };
+
+	/* forward declaration */
+	class parser;
+
+
+	class msg {
+
+		// -- friends ---------------------------------------------------------
+
+		/* parser as friend */
+		friend class parser;
+
+
+		public:
+
+			// -- public types -----------------------------------------------
+
+			/* tags type */
+			typedef std::vector<std::pair<std::string, std::string> > tags;
+
+			/* source type */
+			typedef std::string source;
+
+
+			/* command type */
+			typedef std::string command;
+
+			/* params type */
+			typedef std::vector<std::string> params;
+
+
+			/* nick type */
+			typedef std::string nick;
+
+			/* user type */
+			typedef std::string user;
+
+			/* host type */
+			typedef std::string host;
+
+
+			// -- public constructors -----------------------------------------
+
+			/* default constructor */
+			msg(void);
+
+			/* copy constructor */
+			msg(const std::string&);
+
+			/* destructor */
+			~msg(void);
+
+
+			// -- public accessors --------------------------------------------
+
+			/* have tags */
+			bool have_tags(void) const;
+
+			/* have source */
+			bool have_source(void) const;
+
+			/* have command */
+			bool have_command(void) const;
+
+			/* have params */
+			bool have_params(void) const;
+
+			/* have nick */
+			bool have_nick(void) const;
+
+			/* have user */
+			bool have_user(void) const;
+
+			/* have host */
+			bool have_host(void) const;
+
+
+			/* get tags */
+			const tags& get_tags(void) const;
+
+			/* get source */
+			const source& get_source(void) const;
+
+			/* get command */
+			const command& get_command(void) const;
+
+			/* get params */
+			const params& get_params(void) const;
+
+			/* get nick */
+			const nick& get_nick(void) const;
+
+			/* get user */
+			const user& get_user(void) const;
+
+			/* get host */
+			const host& get_host(void) const;
+
+
+		private:
+
+			// -- private members ---------------------------------------------
+
+			/* tags */
+			tags    _tags;
+
+			/* source */
+			source  _source;
+
+			/* command */
+			command _command;
+
+			/* params */
+			params  _params;
+
+			/* nick */
+			nick    _nick;
+
+			/* user */
+			user    _user;
+
+			/* host */
+			host    _host;
+
+	};
+
+
+    class parser {
+
+        public:
+
+			// -- public static methods ---------------------------------------
+
+			/* parse */
+            static irc::msg parse(const std::string&);
+
+
+        private:
+
+			// -- private constructors ----------------------------------------
+
+			/* default constructor */
+            parser(void);
+
+			/* copy constructor */
+			parser(const parser&);
+
+			/* destructor */
+			~parser(void);
+
+    };
 }
+
+#endif
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,30 +1,195 @@
 #include "parser.hpp"
 
-//irc::Parser::Parser(const Lexer& lexer)
-//: _lexer(lexer), _currToken(_lexer.getNextToken()) {
-//
-//    return ;
-//}
-//
-//std::vector< std::vector< irc::Token > > irc::Parser::parse(void) {
-//
-//    std::vector< std::vector< Token > > matrix;
-//    std::vector<Token> cmd;
-//
-//    while (_currToken.getType() != EOF_TOKEN) {
-//
-//        while (_currToken.getType() != NEWLINE && _currToken.getType() != EOF_TOKEN) {
-//
-//            cmd.push_back(_currToken);
-//            _currToken = _lexer.getNextToken();
-//
-//        }
-//
-//        matrix.push_back(cmd);
-//        cmd.clear();
-//        _currToken = _lexer.getNextToken();
-//
-//    }
-//
-//    return matrix;
-//}
+
+// -- M S G -------------------------------------------------------------------
+
+
+// -- public constructors -----------------------------------------------------
+
+/* default constructor */
+irc::msg::msg(void)
+: _tags(),
+  _source(),
+  _command(),
+  _params(),
+  _nick(),
+  _user(),
+  _host() {
+}
+
+/* copy constructor */
+irc::msg::msg(const std::string& msg) {
+}
+
+/* destructor */
+irc::msg::~msg(void) {
+}
+
+
+// -- public accessors --------------------------------------------------------
+
+/* have tags */
+bool irc::msg::have_tags(void) const {
+	return !_tags.empty();
+}
+
+/* have source */
+bool irc::msg::have_source(void) const {
+	return !_source.empty();
+}
+
+/* have command */
+bool irc::msg::have_command(void) const {
+	return !_command.empty();
+}
+
+/* have params */
+bool irc::msg::have_params(void) const {
+	return !_params.empty();
+}
+
+/* have nick */
+bool irc::msg::have_nick(void) const {
+	return !_nick.empty();
+}
+
+/* have user */
+bool irc::msg::have_user(void) const {
+	return !_user.empty();
+}
+
+/* have host */
+bool irc::msg::have_host(void) const {
+	return !_host.empty();
+}
+
+
+/* get tags */
+const irc::msg::tags& irc::msg::get_tags(void) const {
+	return _tags;
+}
+
+/* get source */
+const irc::msg::source& irc::msg::get_source(void) const {
+	return _source;
+}
+
+/* get command */
+const irc::msg::command& irc::msg::get_command(void) const {
+	return _command;
+}
+
+/* get params */
+const irc::msg::params& irc::msg::get_params(void) const {
+	return _params;
+}
+
+/* get nick */
+const irc::msg::nick& irc::msg::get_nick(void) const {
+	return _nick;
+}
+
+/* get user */
+const irc::msg::user& irc::msg::get_user(void) const {
+	return _user;
+}
+
+/* get host */
+const irc::msg::host& irc::msg::get_host(void) const {
+	return _host;
+}
+
+
+// -- P A R S E R -------------------------------------------------------------
+
+
+// -- private constructors ----------------------------------------------------
+
+/* default constructor */
+irc::parser::parser(void) {
+}
+
+/* copy constructor */
+irc::parser::parser(const irc::parser& parser) {
+}
+
+/* destructor */
+irc::parser::~parser(void) {
+}
+
+
+static bool not_space(int ch) {
+	return !std::isspace(ch);
+}
+
+
+// -- public static methods ---------------------------------------------------
+
+/* parse */
+irc::msg irc::parser::parse(const std::string& msg) {
+
+	//std::vector<irc::token> tokens;
+
+	irc::msg message;
+
+	// trim leading and trailing white spaces
+	std::string trim_line = msg;
+
+	trim_line.erase(trim_line.begin(), std::find_if(trim_line.begin(), trim_line.end(), not_space));
+
+	trim_line.erase(std::find_if(trim_line.rbegin(), trim_line.rend(), not_space).base(), trim_line.end());
+
+	// loop over the message
+	for (std::string::size_type i = 0; i < trim_line.size(); ++i) {
+
+		// tag parsing
+		if (trim_line[i] == '@') {
+			// parse tags
+			while (trim_line[i] != ' ' && i < trim_line.size()) {
+				std::string tag;
+				while (trim_line[i] != ';' && trim_line[i] != ' ' && i < trim_line.size()) {
+					tag += trim_line[i++];
+				}
+				std::string::size_type separator_pos = tag.find('=');
+				if (separator_pos != std::string::npos) {
+					std::string key = tag.substr(0, separator_pos);
+					std::string value = tag.substr(separator_pos + 1);
+					message._tags.push_back(std::make_pair(key, value));
+				}
+				if (i < trim_line.size() && trim_line[i] == ';') ++i;
+			}
+		}
+
+		// source parsing
+		if (trim_line[i] == ':') {
+			// parse source
+			++i;  // skip ':'
+			while (trim_line[i] != ' ' && i < trim_line.size()) {
+				message._source += trim_line[i++];
+			}
+		}
+
+
+		// command parsing
+		if (trim_line[i] == ' ' || i == trim_line.size()) {
+			// parse command
+			while (trim_line[i] != ' ' && i < trim_line.size()) {
+				message._command += trim_line[i++];
+			}
+		}
+
+		// parameters parsing
+		while (i < trim_line.size()) {
+			std::string param;
+			while (trim_line[i] != ' ' && i < trim_line.size()) {
+				param += trim_line[i++];
+			}
+			message._params.push_back(param);
+			if (i < trim_line.size() && trim_line[i] == ' ') ++i;
+		}
+
+
+	}
+
+	return message;
+}
